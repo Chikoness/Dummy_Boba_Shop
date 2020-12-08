a909a64f5f2c0354586bfc391ca22c15ddc7f9e4
Charlene A <chichiko90@gmail.com>
2020-12-08 23:48:54 +0800

    Continuing tutorial at navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,14 +1,21 @@
 import React from 'react'
-import {Nav, NavbarContainer, NavLogo} from './NavbarElements'
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks } from './NavbarElements'
+import MenuIcon from '@material-ui/icons/Menu';
 
 const Navbar = () => {
     return (
         <>
             <Nav>
                 <NavbarContainer>
-                    <NavLogo>                     
-                        Boba
-                    </NavLogo>
+                    <NavLogo to='/'>Boba</NavLogo>
+                        <MobileIcon>
+                            <MenuIcon />
+                        </MobileIcon>
+                        <NavMenu>
+                            <NavItem>
+                                <NavLinks to='about'>About</NavLinks>
+                            </NavItem>
+                        </NavMenu>
                 </NavbarContainer>
             </Nav>
         </>
