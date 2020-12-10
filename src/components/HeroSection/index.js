85096cde8eca5630a5713e2fc7ffa4ba95aca431
Charlene A <chichiko90@gmail.com>
2020-12-10 18:40:12 +0800

    More styling on navbar, sidebar and herosection

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,13 +1,23 @@
 import React from 'react'
-import { HeroBg, HeroContainer, ImgBg } from './HeroElements'
+import { HeroBg, HeroContainer, HeroContent, HeroH1, HeroP, ImgBg } from './HeroElements'
 import CoverPicture from '../../images/boba-cover.jpg'
+import CoverPicture2 from '../../images/boba2.jpg'
 
 const HeroSection = () => {
     return (
         <HeroContainer>
             <HeroBg>
                 <ImgBg src={CoverPicture} />
+                <ImgBg src={CoverPicture2} />
             </HeroBg>
+            <HeroContent>
+                <HeroH1>Welcome to Boba Shop!</HeroH1>
+                <HeroP>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                </HeroP>
+            </HeroContent>
         </HeroContainer>
     )
 }
